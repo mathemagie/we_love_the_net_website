c769cc5aafb2fb1e796f67373d2a1460791daaa5
Fache aurélien <mathemagie@Faches-MacBook-Pro.local>
2014-03-30 18:06:50 +0200

    add footer and share in catchbarge

--- share_bloc.php
+++ share_bloc.php
@@ -1,2 +1,5 @@
+<?php
+	$monUrl = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; 
+?>
 <a class='share'  href="http://www.twitter.com/share?text=<?php echo urlencode($title);?>"><i class="fa fa-twitter"></i></a>
-<a class='share' href="https://www.facebook.com/sharer/sharer.php?t=<?php echo urlencode($title);?>"><i class="fa fa-facebook"></i></a>
+<a class='share' href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode($monUrl);?>"><i class="fa fa-facebook"></i></a>
