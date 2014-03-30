746b6be43c34ec274ef62baacd03b3fc03d014a5
Fache aurélien <mathemagie@Faches-MacBook-Pro.local>
2014-03-30 18:37:22 +0200

    add share bloc on all projects

--- share_bloc.php
+++ share_bloc.php
@@ -1,5 +1,5 @@
 <?php
 	$monUrl = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; 
 ?>
-<a class='share' target='_new' href="http://www.twitter.com/share?text=<?php echo urlencode($title);?>"><i class="fa fa-twitter"></i></a>
-<a class='share' target='_new' href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode($monUrl);?>"><i class="fa fa-facebook"></i></a>
+<a class='share' target='_new' href="http://www.twitter.com/share?text=<?php echo urlencode($title);?>"><i class="fa fa-twitter fa-2x"></i></a>
+&nbsp;<a class='share' target='_new' href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode($monUrl);?>"><i class="fa fa-facebook fa-2x"></i></a>
