e72adc44d06967bf71c6d1a78759d53a3c07601b
Fache aurélien <mathemagie@Faches-MacBook-Pro.local>
2014-03-30 15:03:20 +0200

    some improvments

--- header.php
+++ header.php
@@ -1,15 +1,12 @@
 <?php 
-
-function isMobile() {
-    return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+	function isMobile() {
+	    return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
 }
-
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8" />
-<!-- le référencement web pour google il faut completer et remplire a la place des # -->
 		<meta name="description" lang="fr" content="<?php echo $title;?>"/>
 		<meta name="keywords" lang="fr"  content="Microtruc, #happyTrucs, data, API, bus, We Love The Net, we love the net, welovethenet, La Tapisserie, la tapisserie, tapisserie, latapisserie, wln,"/>
 		<meta name="author" content="Bastien DIDIER, We Love the Net"/>
@@ -19,14 +16,8 @@ function isMobile() {
         <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <link rel="stylesheet" href="/css/font-awesome.min.css">
-
 		<title><?php echo $title;?></title>
-		<script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
-<script type="text/javascript">stLight.options({publisher: "c00ea4c5-5727-4fb5-ae73-343dcd818626", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
-
-	  	<link rel="stylesheet" type="text/css" href="/css/normalize.css"/>
-      <link rel="stylesheet" type="text/css" href="/css/hack.css?v=3"/>
-
-
-<link href="http://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet" type="text/css">
-<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" type="text/css" href="/css/normalize.css"/>
+		<link rel="stylesheet" type="text/css" href="/css/hack.css?v=3"/>
+		<link href="http://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet" type="text/css">
+		<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
--- lieu.php
+++ lieu.php
@@ -1,5 +1,4 @@
 <?php
-
   $title = "lieu";
   require_once './header.php';
 ?>
@@ -10,41 +9,35 @@
           require_once "./sidebar.php";
     ?>
    <div class="col-md-9"><br/>
-     <?php 
-
-          require_once "./nav.php";
+        <?php 
+            require_once "./nav.php";
         ?>
           <p>13 rue Petion, Paris, 11e - contact-at-we-love-the.net</p>
           <p>Rideau ouvert . <span class="small9">nous sommes là, vous pouvez passer </span> /// rideau fermé . <span class="small9">nous ne sommes pas dispo</span></p>
-              <p>
-                <iframe src = "http://api.la-tapisserie.net/expo.html" width = "100%" height = "500px" frameborder = "0" scrolling="no"> </iframe>                
-</p>
-              <table width="800" border="0">
-                <tr>
-                  <td><p><span class="small9">Le lieu tire son nom de sa fonction passée : un tapissier y était installé. <br>
-                  <p>Il est situé au 13 rue Pétion, au centre du 11ème arrondissement, à 2 minutes à pied du métro Voltaire et à 10 minutes du métro Père Lachaise. </p>
-                  <img class="img-responsive" src='http://maps.googleapis.com/maps/api/staticmap?center=13%20rue%20petion%20paris&zoom=16&size=800x300&key=AIzaSyBpRjBLs9Dh70hxOoGLoBIem_0EKwu3z4k&&markers=color:blue|48.859038,2.381283'></img>
-                  <br/>
-                  <p>Il est constitué d&rsquo;un espace de 28 mètres carré, avec vitrine donnant sur la rue, et d&rsquo;un sous-sol de 15 mètres carré environ. </p>
-                  <br>
-                  Le lieu est entièrement modulaire selon les moments, lieu de création, formation, exposition, événement.<br>
-                  </span><br>
-                  <strong><em>Les expositions et évènements de la Tapisserie sur <a href="http://www.la-tapisserie.net/" target="_blank">www.la-tapisserie.net</a></em></strong> </p></td>
-                </tr>
-              </table>
-           <br>
-              Nos équipements<br>
-              <ul>
-                <li>Vitrine sur la rue</li>
-                <li>Espace d'expo : <a href="http://www.la-tapisserie.net/" target="_blank">La tapisserie</a></li>
-                <li>Etablis</li>
-                <li>Outils classiques</li>
-                <li>Fer à souder, multimètre, etc...</li>
-                <li>Machine à chantourner</li>
-                <li>Découpe laser . lasersaur (dans un autre local)</li>
-              </ul>
-	     
-    </div>
+          <p>
+            <iframe src = "http://api.la-tapisserie.net/expo.html" width = "100%" height = "500px" frameborder = "0" scrolling="no"> </iframe>                
+          </p>
+          <p>Le lieu tire son nom de sa fonction passée : un tapissier y était installé. </p>
+            <p>Il est situé au 13 rue Pétion, au centre du 11ème arrondissement, à 2 minutes à pied du métro Voltaire et à 10 minutes du métro Père Lachaise. </p>
+            <img class="img-responsive" src='http://maps.googleapis.com/maps/api/staticmap?center=13%20rue%20petion%20paris&zoom=16&size=800x400&key=AIzaSyBpRjBLs9Dh70hxOoGLoBIem_0EKwu3z4k&&markers=color:blue|48.859038,2.381283'></img>
+            <br/>
+            <p>Il est constitué d&rsquo;un espace de 28 mètres carré, avec vitrine donnant sur la rue, et d&rsquo;un sous-sol de 15 mètres carré environ. </p>
+            <p>Le lieu est entièrement modulaire selon les moments, lieu de création, formation, exposition, événement.</p>
+            <br>
+            <strong><em>Les expositions et évènements de la Tapisserie sur <a href="http://www.la-tapisserie.net/" target="_blank">www.la-tapisserie.net</a></em></strong> </p>
+      
+       <br>
+          Nos équipements<br>
+          <ul>
+            <li>Vitrine sur la rue</li>
+            <li>Espace d'expo : <a href="http://www.la-tapisserie.net/" target="_blank">La tapisserie</a></li>
+            <li>Etablis</li>
+            <li>Outils classiques</li>
+            <li>Fer à souder, multimètre, etc...</li>
+            <li>Machine à chantourner</li>
+            <li>Découpe laser . lasersaur (dans un autre local)</li>
+          </ul>
+	   </div>
    </div>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
