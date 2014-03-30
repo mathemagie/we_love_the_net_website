44dbeb4c394c32b384b88b9ae4b92c575c41141e
Fache aurélien <mathemagie@Faches-MacBook-Pro.local>
2014-03-30 15:30:34 +0200

    add new share bloc

---
+++ share_bloc.php
@@ -0,0 +1,2 @@
+<a class='share'  href="http://www.twitter.com/share?text=<?php echo urlencode($title);?>"><i class="fa fa-twitter"></i></a>
+<a class='share' href="https://www.facebook.com/sharer/sharer.php?t=<?php echo urlencode($title);?>"><i class="fa fa-facebook"></i></a>
