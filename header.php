27eb27ad9e86e297ad4ec6e1131042d58dc98531
Fache aurélien <mathemagie@Faches-MacBook-Pro.local>
2014-04-01 15:04:45 +0200

    add fixed sidebar

--- header.php
+++ header.php
@@ -20,5 +20,5 @@
     <link rel="stylesheet" href="/css/font-awesome.min.css">
 	<title><?php echo $title;?></title>
 	<link rel="stylesheet" type="text/css" href="/css/normalize.css"/>
-	<link rel="stylesheet" type="text/css" href="/css/hack.css?v=18"/>
+	<link rel="stylesheet" type="text/css" href="/css/hack.css?v=20"/>
 	<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
