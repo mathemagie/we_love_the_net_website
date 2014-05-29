5ba1add7683882b3a454736f16b7994c89c5d9e5
Fache aurélien <mathemagie@Faches-MacBook-Pro.local>
2014-05-29 17:30:51 +0200

    new version of happytrucs listing

--- productions.php
+++ productions.php
@@ -2,6 +2,7 @@
   $title = "Productions";
   require_once './header.php';
 ?>
+<link rel="stylesheet" type="text/css" href="css/jquery.hoverGrid.css">
 <body>
  <div id='wrapper' class="container-fluid">
   <div class="row">
@@ -9,6 +10,7 @@
           require_once "./sidebar.php";
     ?>
      <div class="col-md-10"><br/>
+
      <?php 
           require_once "./nav.php";
         ?>
@@ -20,8 +22,59 @@
               <p class="small9">(*)  branchés à internet<br>
                 <br>
               </p>
-              <p>La série est actuellement composée de 6 objets prototypes&nbsp;:</p>
-              <ul>
+              <div>
+                  <p>La série est actuellement composée de 6 objets prototypes&nbsp;:</p>
+                <div id="all_happyTrucs">
+                    <div class="item">
+                        <a href="/7ieme-ciel"><img src="images/7eme-ciel-1.jpg" alt="7ème ciel" title="7ème ciel" /></a>
+                        <div class="caption" style="display: none;">
+                            <h2>7ieme ciel</h2>
+                            <p>Des objets mobiles personnalisés montent et descendent le long d’un fil en fonction de l’altitude physique temps réel des personnes du lieu ou des habitués du lieu.</p>
+                        </div>
+                    </div>       
+                    <div class="item">
+                        <a href="/le-catch-barge"><img src="le-catch-barge/3.jpg" alt="le CatchBarge" title="CatchBarge" /></a>
+                        <div class="caption" style="display: none;">
+                            <h2>CatchBarge</h2>
+                            <p>Le CatchBarge est une installation destinée aux vitrines des lieux et conçue pour attirer le chaland.
+                Cette installation est prévue pour être pilotée de l’extérieur, de l’autre côté de la vitrine par le chaland.</p>
+                        </div>
+                    </div>                 
+               
+                 <div class="item">
+                        <a href="/little-umbrella/"><img src="/little-umbrella/little-3-umbrella.jpg" alt="Little Umbrella" title="Little Umbrella" /></a>
+                        <div class="caption" style="display: none;">
+                            <h2>Litte Umbrella</h2>
+                            <p>Little Umbrella est un petit parapluie connecté à Internet qui permet de connaître le temps qu'il va faire dans les 6 prochaines heures</p>
+                        </div>
+                    </div>                 
+                  <div class="item">
+                        <a href="/tweet-place"><img src="http://api.la-tapisserie.net/rideau/6.jpg" alt="TweetPlace" title="TweetPlace" /></a>
+                        <div class="caption" style="display: none;">
+                            <h2>TweetPlace</h2>
+                            <p>Tweet Place est un dispositif connecté permettant de savoir à distance si un lieu est ouvert ou fermé</p>
+                        </div>
+                    </div>                 
+                
+                <div class="item">
+                        <a href="/arret-demande/"><img width="620" src="images/arret_demande.jpg" alt="Arrêt demandé" title="Arrêt demandé" /></a>
+                        <div class="caption" style="display: none;">
+                            <h2>Arrêt demandé</h2>
+                            <p><em>Arrêt Demandé</em> est un objet destiné à annoncer de manière visuelle ou sonore l'arrivée prochaine de votre bus.</p>
+                        </div>
+                    </div>                 
+                </div>
+                <br clear="all"><br/>
+
+                 <ul>
+                <li><a href="/agit-prop/"><strong>AgitProp</strong></a><strong><br>
+                </strong><em>AgitProp</em> est un objet sensible à un&lt;mot clé&gt; choisi.<br>
+                Prévu pour être installé in situ dans un lieu, AgitProp  présente un objet qui s&rsquo;anime dans le cas de la publication sur Google, pendant la dernière heure passée, d&rsquo;un article avec le &lt;mot clé&gt; choisi.              </li>
+              </ul>
+              <br/>
+
+            
+             <!-- <ul>
                 <li><a href="/tweet-place/"><strong>Tweet Place</strong></a><strong><br>
                 </strong><em>Tweet Place</em> est un dispositif connecté permettant de savoir à distance si un lieu est ouvert ou fermé<br>
                 </li>
@@ -34,11 +87,7 @@
                 <li><a href="/little-umbrella/"><strong>Little Umbrella</strong></a><strong><br>
                 </strong><em>Little Umbrella</em> est un petit parapluie connecté à Internet qui permet de connaître le temps qu'il va faire dans les 6 prochaines heures</li>
               </ul>
-              <ul>
-                <li><a href="/agit-prop/"><strong>AgitProp</strong></a><strong><br>
-                </strong><em>AgitProp</em> est un objet sensible à un&lt;mot clé&gt; choisi.<br>
-                Prévu pour être installé in situ dans un lieu, AgitProp  présente un objet qui s&rsquo;anime dans le cas de la publication sur Google, pendant la dernière heure passée, d&rsquo;un article avec le &lt;mot clé&gt; choisi.              </li>
-              </ul>
+             
               <ul>
                 <li><a href="/le-catch-barge/"><strong>Le CatchBarge</strong></a><strong><br>
                 </strong><em>Le CatchBarge</em> est une installation destinée aux vitrines des lieux et conçue pour attirer le chaland (d&rsquo;où le nom de Catch Barge&nbsp;: Attrape Chaland en français).<br>
@@ -48,7 +97,7 @@
                 <li><a href="/arret-demande/"><strong>Arrêt Demandé</strong></a><strong></strong><br>
                   <em>Arrêt Demandé</em> est un objet destiné à annoncer de manière visuelle ou sonore l'arrivée prochaine de votre bus.              </li>
               </ul>
-              <p>&nbsp;</p>
+              <p>&nbsp;</p>-->
               <h5><i class="fa fa-chevron-right"></i> Les autres objets</h5>
               <ul>
                 <li>la Boîte  Meuh</strong>
@@ -66,7 +115,14 @@ interactive                    </p></li>
    </div>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="/js/jquery.hoverGrid.js"></script>
 <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+  <script type="text/javascript">
+    $(document).ready(function() {
+      $('#all_happyTrucs').hoverGrid();
+    });
+</script>
+
 <?php require_once './footer.php';?>
 </body>
 </html>
