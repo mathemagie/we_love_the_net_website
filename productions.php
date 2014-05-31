11d3609d0e4d8ec9ba6dd33a3087c1eaf7baa29b
Fache aurélien <mathemagie@Faches-MacBook-Pro.local>
2014-05-31 17:07:08 +0200

    add croisements in productions page

--- productions.php
+++ productions.php
@@ -32,8 +32,7 @@
                   <h3>CatchBarge</h3>
                   <p>Le CatchBarge est une installation destinée aux vitrines des lieux et conçue pour attirer le chaland.
                   Cette installation est prévue pour être pilotée de l’extérieur, de l’autre côté de la vitrine par le chaland.</p>
-
-                  <a href="/le-catch-barge"><img width="610px" height="400px"  class="img-responsive" src="le-catch-barge/3.jpg" alt="le CatchBarge" title="CatchBarge" /></a>
+                   <a href="/le-catch-barge"><img width="610px" height="400px"  class="img-responsive" src="le-catch-barge/3.jpg" alt="le CatchBarge" title="CatchBarge" /></a>
                    <br/><br/>
                   <h3>Litte Umbrella</h3>
                   <p>Little Umbrella est un petit parapluie connecté à Internet qui permet de connaître le temps qu'il va faire dans les 6 prochaines heures</p>
@@ -46,12 +45,12 @@
                   <h3>Arrêt demandé</h3>
                   <p><em>Arrêt Demandé</em> est un objet destiné à annoncer de manière visuelle ou sonore l'arrivée prochaine de votre bus.</p>
                   <a href="/arret-demande/"><img width="610px" height="400px" class="img-responsive" src="images/arret_demande.jpg" alt="Arrêt demandé" title="Arrêt demandé" /></a>
-                   <h3>Agit Prop</h3>
-                    <p>AgitProp est un objet sensible à un&lt;mot clé&gt; choisi.<br>
-                    Prévu pour être installé in situ dans un lieu, AgitProp  présente un objet qui s&rsquo;anime dans le cas de la publication sur Google, pendant la dernière heure passée, d&rsquo;un article avec le &lt;mot clé&gt; choisi.</p>
-                     <a href="/agit-prop/"><img width="610px" height="400px" class="img-responsive" src="images/pegman.jpg" alt="Pegman" title="Pegman" /></a>
+                  <h3>Agit Prop</h3>
+                  <p>AgitProp est un objet sensible à un&lt;mot clé&gt; choisi.<br>
+                  Prévu pour être installé in situ dans un lieu, AgitProp  présente un objet qui s&rsquo;anime dans le cas de la publication sur Google, pendant la dernière heure passée, d&rsquo;un article avec le &lt;mot clé&gt; choisi.</p>
+                  <a href="/agit-prop/"><img width="610px" height="400px" class="img-responsive" src="images/pegman.jpg" alt="Pegman" title="Pegman" /></a>
 
-            
+
              <!-- <ul>
                 <li><a href="/tweet-place/"><strong>Tweet Place</strong></a><strong><br>
                 </strong><em>Tweet Place</em> est un dispositif connecté permettant de savoir à distance si un lieu est ouvert ou fermé<br>
@@ -76,20 +75,22 @@
                   <em>Arrêt Demandé</em> est un objet destiné à annoncer de manière visuelle ou sonore l'arrivée prochaine de votre bus.              </li>
               </ul>
               <p>&nbsp;</p>-->
-              <br/>
-              <h5><i class="fa fa-chevron-right"></i> Les autres objets</h5>
+              <!--<h5><i class="fa fa-chevron-right"></i> Les autres objets</h5>
               <ul>
                 <li>la Boîte  Meuh</strong>
 interactive                    </p></li>
               </ul>
-              <p><br>
+              <p><br>-->
+                <br/><br/>
             </p>
-              <h5><i class="fa fa-chevron-right"></i>&nbsp;Les installations artistiques</h5>
-              <ul>
+              <h5><i class="fa fa fa-hand-o-right fa-2x"></i>&nbsp;Les installations artistiques</h5>
+             
                 
-                <li><a href="http://we-love-the.net/7ieme-ciel/"><strong>7ième Ciel</strong></a></li>
-                <li><a href="http://albertinemeunier.net/croisements/">Croisements</a></li>
-              </ul>
+                <!--<li><a href="http://albertinemeunier.net/croisements/">Croisements</a></li>-->
+                    <h3>Croisements</h3>
+                  <p>Se croiser au hasard d'une rue, d'un café, avoir la chance de se rencontrer par hasard... Croisements est né de l'observation répétée de nos déplacements dans la ville et dans notre vie de tous les jours.</p>
+                  <a href="http://albertinemeunier.net/croisements/"><img width="610px" height="400px" class="img-responsive" src="http://albertinemeunier.net/croisements/photos/croisements-1.jpg" alt="Pegman" title="Pegman" /></a>
+              
         </div>
    </div>
 </div>
