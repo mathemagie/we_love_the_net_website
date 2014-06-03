b48a20f77bbe538ce7204477c10b595b311ee901
Fache aurélien <aurelien.fache@gmail.com>
2014-06-03 11:30:04 +0200

    add annonce

--- index.php
+++ index.php
@@ -24,9 +24,11 @@
 		        Réalisation, diffusion et promotion de projets liés au design et à l&rsquo;art numérique, <br>
 	          et accompagnement de publics vers les NTIC</p>
 		      <p>&nbsp;</p>
-              <p><strong><i class="fa fa-chevron-right"></i>&nbsp;Nous recherchons 16 dames de plus de 66 ans </strong>. <a href="http://labotrucs.tumblr.com/post/72654209859/petite-annonce-nous-recherchons-des-dames-de-plus-de">voir l'annonce</a></p>
+              <p><strong><i class="fa fa-chevron-right"></i>&nbsp;Nous recherchons 16 dames de plus de 66 ans </strong>. <a target='_blank' href="http://labotrucs.tumblr.com/post/72654209859/petite-annonce-nous-recherchons-des-dames-de-plus-de">voir l'annonce</a></p>
+
 <!--		      <p><strong>&gt; nous recherchons des co-locataires : développeur, designer, bidouilleur, artiste, graphiste ... </strong><a href="http://we-love-the.net/annonce/">voir l'annonce</a>		      <br>-->
-		        
+		       <p><strong><i class="fa fa-chevron-right"></i>&nbsp;Nous recherchons des enfants de 8 à 10 ans </strong>. <a target='_blank' href="http://labotrucs.tumblr.com/post/87680655214/petite-annonce-nous-recherchons-des-enfants-de-8-a-10">voir l'annonce</a></p>
+  
               </p>
 		      <p><i class="fa fa-chevron-right"></i> et puis aussi ... <br>
 	          <img class="img-responsive" src="images/cherche-hacker.jpg" width="650" height="137" alt="cherche hacker"></p>
