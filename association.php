51f8ca21f5ca683d9cb0a15a942c7384a0c8a85b
Fache aurélien <mathemagie@Faches-MacBook-Pro.local>
2014-11-18 23:44:14 +0100

    fix typo

--- association.php
+++ association.php
@@ -77,27 +77,26 @@ et accompagnement de publics vers les NTIC</p>
            <br/>
             <h2>Les membres actifs</h2>
             <br/>
+                        <h4>Bastien Didier</h4>
+            <p>Artiste des Nouveaux médias, Bastien DIDIER est One Billion Cat, un étudiant en design graphique numérique basé à Paris. Fort du bagage historique de son établissement, l'école du livre Estienne, il cherche à appliquer les règles parfois rigides de la typographie traditionnelle et de la mise en page à Internet. A contrario, dans ces travaux papier, Bastien est surtout poussé par l'envie d'aller au-delà de ce cadre rigide en prônant une liberté dans la forme comparable à celle que l'on peut habituellement trouver sur internet. Né en même temps qu'internet, il en connaît les enjeux et cherche sans cesse à en repousser les frontières. Adepte, de technologie, il ne donne pas de limites à son travail et aborde de nombreux médias. Bastien, c'est donc un point de vue résolument porté vers l'avenir, une armée que chacun est amené à rejoindre, l'armée du progrès. <a href="http://one-billion-cat.tumblr.com/" class="small9">one-billion-cat.tumblr.com</a></p>
+             
+              <br>
+
             <h4>Aurélien Fache <a class='share' href="http://twitter.com/mathemagie"><i class="fa fa-twitter"></i></a>&nbsp;<a class='share' href="http://fr.linkedin.com/in/aurelienfache/"><i class="fa fa-linkedin"></i></a></h4>
               <!--<img style="float:left" width='120px' height='120px'src="http://mathemagie.net/me.jpg"/>-->
               <p>Archéologue du web (Multimania, Lycos, Dailymotion, Netvibes), co-fondateur et ancien directeur technique d'OWNI (media en ligne sur les cultures numériques), Aurélien s'intéresse aujourd'hui à tout qui concerne les APIs, l'internet des objets et la Réalité Augmentée.</p>
               <em>Le développeur le plus créatif de la place de Paris</em>, dixit albertine  <br>
               <a href="http://mathemagie.net">mathemagie.net</a>
-              <br><br/><br/>
+              <br><br/>
               
-            <p><h4>Carola Moujan <a class='share' href="https://twitter.com/CarolaMoujan"><i class="fa fa-twitter"></i></a></h4>
-              <p>est designer, artiste et chercheur, docteur en Art et Sciences de l’art de l’Université Paris 1 (Sorbonne). Après une formation initiale d'architecte et de plasticienne et des débuts comme directrice artistique dans la presse et la communication, son travail s’oriente dès 1997 vers la création numérique. Sa pratique professionnelle investit plusieurs champs dont le design d’interaction, le graphisme, le mobilier urbain et l’installation. Elle a publié de nombreux articles sur l'esthétique des environnements numériquement augmentés. Ses recherches actuelles se développent autour de trois axes : l’expérience de l’espace à l’âge du numérique, les interactions hors-écran, la théorie du design. Elle enseigne la théorie et la pratique du design numérique à l’École Camondo et à l’Université de Valenciennes. <a href="http://www.carolamoujan.net/" target="_blank" class="small9">www.carolamoujan.net</a><br>
-            </p><br>
-          
-            <h4>Loic Le guen <a class='share' href="https://twitter.com/oicleguen"><i class="fa fa-twitter"></i></a></h4><span class="">Designer . </span><a href="http://www.loicleguen.fr/" class="small9">www.loicleguen.fr</a><br>
-            <br>
             </p>
           <p><h4>Cristina Hoffmann.</h4>
             <p>Espagnole, née à mexico en 1981, elle vit et travaille à Paris. Elle découvre le design pendant ses études d'ingénieur, lorsqu'elle se questionne sur la technologie: son sens, la place qu'elle donne à l'humain et son impact. Pendant dix ans elle travaille à la conception de produits et services numériques dans le milieu académique et industriel. Elle intervient aussi dans des colloques internationaux sur le design et l'innovation. En 2013 elle a mis en pause son travail de designer chez Orange pour réinvestir sa création personnelle. Elle s'intéresse à la nature humaine, l'inexprimable et la connexion, et sa recherche plastique vise à faire interagir des outils et supports numériques avec le dessin et la peinture.</p>
+
             <br/>
-            <h4>Bastien Didier</h4>
-            <p>Artiste des Nouveaux médias, Bastien DIDIER est One Billion Cat, un étudiant en design graphique numérique basé à Paris. Fort du bagage historique de son établissement, l'école du livre Estienne, il cherche à appliquer les règles parfois rigides de la typographie traditionnelle et de la mise en page à Internet. A contrario, dans ces travaux papier, Bastien est surtout poussé par l'envie d'aller au-delà de ce cadre rigide en prônant une liberté dans la forme comparable à celle que l'on peut habituellement trouver sur internet. Né en même temps qu'internet, il en connaît les enjeux et cherche sans cesse à en repousser les frontières. Adepte, de technologie, il ne donne pas de limites à son travail et aborde de nombreux médias. Bastien, c'est donc un point de vue résolument porté vers l'avenir, une armée que chacun est amené à rejoindre, l'armée du progrès. <a href="http://one-billion-cat.tumblr.com/" class="small9">one-billion-cat.tumblr.com</a></p>
-             
-              <br>
+                        <h4>Loic Le guen <a class='share' href="https://twitter.com/oicleguen"><i class="fa fa-twitter"></i></a></h4><span class="">Designer . </span><a href="http://www.loicleguen.fr/" class="small9">www.loicleguen.fr</a><br>
+            <br>
+
 
             <h4>Thu Trinh-Bouvier <a class='share' href="https://twitter.com/T_Tiou"><i class="fa fa-twitter"></i></a>&nbsp;<a class='share' href="http://t-tiou.tumblr.com/"><i class="fa fa-tumblr"></i></a></h4>
             <p>Diplômée de l’ENSAD (Ecole des Art Décoratif de Paris), elle étudie actuellement la place et la fonction de l’image dans les réseaux sociaux dans le cadre d’un « executive master en communication » à Sciences Po.
