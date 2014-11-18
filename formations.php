825d7e8632868ab99d0885e0b9279e4638583708
Fache aurélien <mathemagie@Faches-MacBook-Pro.local>
2014-11-18 23:51:52 +0100

    fix type

--- formations.php
+++ formations.php
@@ -2,33 +2,60 @@
   $title = "Formations";
   require_once './header.php';
 ?>
-	<body>
-
-		 <div id='wrapper' class="container-fluid">
-	<div class="row">
-	<?php 
+<body>
+ <div id='wrapper' class="container-fluid">
+  <div class="row">
+     <?php 
           require_once "./sidebar.php";
-    ?>
-	 <div class="col-md-10"><br/>
-	 	 <?php 
-		      require_once "./nav.php";
-		    ?>
-		      <br/>
-	          13 rue Petion, Paris, 11e - contact-AT-we-love-the.net<br>
-              <br/>
-		      <p>Réalisation, diffusion et promotion de projets liés au design et à l&rsquo;art numérique, <br>
-et accompagnement de publics vers les NTI</p>
-		      <ul>
-				<li>physical computing (arduino, raspberry pi)</li>
-				<li>API</li>
-			     <li>HTML5, Javascript, Php, Python</li>
-			   </ul>
-            <p><img class="img-responsive" src="images/carlson-1-s.jpg" width="600" height="401"></p>
-	  </div>
-	 </div>
+        ?>
+    <div class="col-md-10"><br/>
+     <?php 
+          require_once "./nav.php";
+        ?>
+              <strong>Workshops &amp; Formations by We Love The Net</strong> </p>
+              
+         <p><strong><img src="images-site/fromation.jpg" width="600" height="450"><br>
+         </strong></p>
+         <p><strong>Qui sommes nous ?</strong></p>
+         <p>Se familiariser avec Internet, fabriquer des objets connectés, l'impression 3D, utiliser des APIs…<br>
+           L'association We Love The Net donne accès grâce à des ateliers, workshops et  formations  aux nouvelles technologies par une approche artistique et ludique. Son approche pédagogique permet de se familiariser avec Internet, de fabriquer des objets connectés, de découvrir l'impression 3D, d'utiliser des APIs…<br>
+         </p>
+         <p>Nous avons de nombreuses compétences dans l'accompagnement et la formation de  publics très divers. Notre plus :  œuvrer et apprendre  les nouvelles technologies par le biais de la création artistique. Des dames de plus de 77 ans jouent de l'internet (<a href="http://www.hyperolds.com" target="_blank">Hype(r)Olds</a>), des enfants de 8 à 10 ans fabriquent des machines inutiles  (<a href="http://labotrucs.tumblr.com/" target="_blank">LabOTrucs</a>), les adultes  manient impression 3D, les Apis, les objets connectés (<a href="http://www.we-love-the.net/productions.php" target="_blank">Happy Trucs</a>)</p>
+<p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p><strong>Les formations proposées :</strong></p>
+      <p>&gt; <strong><a href="workshop-atelier-formation-entreprises.html" target="_self">Pour les entreprises</a></strong><br>
+  Créer un parapluie connecté, récupérer des données via Internet (APIs), réaliser une vitrine interactive…<br>
+  D'une demi journée jusqu'à 5 jours, We LoveThe Net vous propose différents modules de formation: <a href="workshop-atelier-formation-entreprises.html" target="_self">+ de détails</a></p>
+<p>&gt; <strong><a href="workshop-atelier-formation-enfants.html" target="_self">Pour les enfants (8 à 10 ans)</a></strong><br>
+   Se familiariser avec la fabrication numérique, fabriquer des machines inutiles, jouer avec des pingouins connectés, ,...<br>
+  We LoveThe Net vous propose des ateliers de 3 h  : <a href="workshop-atelier-formation-enfants.html" target="_self">+ de détails</a></p>
+<p>&gt; <strong><a href="workshop-atelier-formation-Personnes-agees.html" target="_self">Pour les personnes âgées</a></strong><br>
+  Apprendre les rudiments de l'Internet et de l'ordinateur, se familiariser avec la fabrication numérique, tricoter le numérique, ...<br>
+   We LoveThe Net vous propose des ateliers de 3 h : <a href="workshop-atelier-formation-Personnes-agees.html" target="_self">+ de détails</a></p>
+<p>&gt; <strong><a href="workshop-atelier-formation-tous-public.html" target="_self">Pour tous les publics</a></strong><br>
+  S'approprier les technologies Hardware (Arduino), construire un objet connecté, comprendre le monde des données…<br>
+  Des ateliers de 3 h jusqu'à trois jours, We LoveThe Net vous propose différents modules de formation: <a href="workshop-atelier-formation-tous-public.html" target="_self">+ de détails</a></p>
+<p>&nbsp;</p>
+<p><strong>Ils nous ont fait confiance :</strong></p>
+<p><img src="images-site/1215623-orange-logo.jpg" width="45" height="45"> <img src="images-site/decalab-logo.png" width="118" height="38"> <img src="images-site/fonderie-logo.png" width="114" height="35"> <img src="images-site/logo-mcd.preview.jpg" width="100" height="45"> <img src="images-site/mairie.jpg" width="83" height="38"></p>
+<hr>
+      <p><strong>Pour nous contacter</strong> : contact-[at]-we-love-the.net</p>
+      <p></p>
+</div>
+   </div>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
-<?php require_once './footer.php';?>
-</body>
+<script>
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-49356734-1', 'we-love-the.net');
+  ga('send', 'pageview');
+
+</script></body>
 </html>
